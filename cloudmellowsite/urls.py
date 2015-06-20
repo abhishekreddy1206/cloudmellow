c80841d2e296cea51ad75b55f2104a6407bc42cb
Abhishek Reddy Yeruva <abhishek@Abhisheks-MacBook-Pro.local>
2015-06-20 18:47:59 -0500

    initial

---
+++ cloudmellowsite/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from . import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'staar.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
+)
